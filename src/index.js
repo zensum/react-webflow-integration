33e13c613f1421443dc3b1d0f3a008fe90222924
Gustav <gustav.gisselfeldt@gmail.com>
2022-10-05 14:45:49 +0200

    wip

--- src/index.js
+++ src/index.js
@@ -4,19 +4,19 @@ import Test from "./test";
 import ReactDOM from "react-dom";
 
 class App extends Component {
-	render() {
-		return (
-			<div id="react-target">
-				<div style={{ background: "yellow" }}>
-					CHANGED AGAIN LOL
-					<Test />
-				</div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div id="react-target">
+        <div style={{ background: "yellow" }}>
+          CHANGED AGAIN LOL
+          <Test />
+        </div>
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(
-	React.createElement(App, {}, null),
-	document.getElementById("react-target"),
+  React.createElement(App, {}, null),
+  document.getElementById("react-target")
 );
