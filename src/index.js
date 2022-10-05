6cc3f565d43a4e95b53482423fd5f9a6e7481b16
mgmichaela <mgmichaelaguzmicka@gmail.com>
2022-10-05 13:47:56 +0200

    update

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ class App extends Component {
       <>
         <div id="react-target" style={{ background: "red" }}>
           Test
-        </div>
 				<test/>
+        </div>
       </>
     );
   }
