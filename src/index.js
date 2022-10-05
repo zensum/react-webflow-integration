511dd403a05f61d5689bdc597b6259df266bbe3c
mgmichaela <mgmichaelaguzmicka@gmail.com>
2022-10-05 09:57:20 +0200

    add bundle, change file

--- src/index.js
+++ src/index.js
@@ -5,7 +5,18 @@ import ReactDOM from 'react-dom'
 class App extends Component {
 	render() {
 		return (
-			<div>This is a React component inside of Webflow!</div>
+			<div>
+				<h1>Testing page</h1>
+				<div id="offer-template" style="display: grid; grid-template-columns: auto auto auto auto auto auto auto auto; grid-template-rows: auto auto;">
+					<img id="partner-logo" style="grid-row: 1 / 3; grid-column: 1 / 3;" height="80" />
+					<span id="partner-name" style="grid-row: 1; grid-column: 3 / 6;">
+						Långivare partnerName
+					</span>
+					<span id="offer-amount" style="grid-row: 2; grid-column: 3 / 6;">
+						Lånebelopp offerAmount
+					</span>
+				</div>
+			</div>
 		)
 	}
 }
