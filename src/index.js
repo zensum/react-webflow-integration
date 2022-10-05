b543db004aaea544e2392bfa7f027d261aedff28
Gustav <gustav.gisselfeldt@gmail.com>
2022-10-05 11:10:36 +0200

    added id

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import ReactDOM from 'react-dom'
+import ReactDOM from "react-dom";
 
 class App extends Component {
-	render() {
-		return (
-			<div style={{background: 'red'}}>Test</div>
-		)
-	}
+  render() {
+    return (
+      <div id="react-target" style={{ background: "red" }}>
+        Test
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(
-	React.createElement(App, {}, null),
-	document.getElementById('react-target')
+  React.createElement(App, {}, null),
+  document.getElementById("react-target")
 );
